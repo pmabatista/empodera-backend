ce042e6148006c0e3f7e01bfd09f10dc473171b4
Pedro Miguel <pedro.042013@gmail.com>
2024-07-05 00:18:31 -0300

    feat: add typerom config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientModule } from './client/client.module';
-import { ContractModule } from './contract/contract.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ClientModule } from './modules/client/client.module';
+import { ContractModule } from './modules/contract/contract.module';
 
 @Module({
-  imports: [ClientModule, ContractModule],
+  imports: [
+    ClientModule,
+    ContractModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/modules/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
