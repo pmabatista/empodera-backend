dd7861e76aa2875cc9e2d991c792e0dc88fe1943
Pedro Miguel <pedro.042013@gmail.com>
2024-07-04 22:25:33 -0300

    feat: add contract and client modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { ContractModule } from './contract/contract.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, ContractModule],
   controllers: [AppController],
   providers: [AppService],
 })
